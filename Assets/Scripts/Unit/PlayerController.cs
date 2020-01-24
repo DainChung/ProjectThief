24170b5917d3d337d3882bef3f16f097afdee758
Dain Jeong <101_silence@naver.com>
2020-01-24 15:25:52 +0900

    캐릭터(카메라) 흔들림 현상 해결, 캐릭터의 비정상적인 회전 해결, 바닥 접촉 판정 변경
    Signed-off-by: Dain Jeong <101_silence@naver.com>

--- Assets/Scripts/Unit/PlayerController.cs
+++ Assets/Scripts/Unit/PlayerController.cs
@@ -13,6 +13,7 @@ namespace Com.MyCompany.MyGame
         private Transform mainCameraTransform;
         private Quaternion destiRotation;
         private Vector3 lookDir;
+        private Vector3 destiPos;
 
         //관성
         private float inertia = 0;
@@ -23,8 +24,6 @@ namespace Com.MyCompany.MyGame
         [HideInInspector]
         public Unit unit;
 
-        [HideInInspector]
-        public Rigidbody rigidBody;
         #endregion
 
         #region MonoBehaviour Callback
@@ -37,7 +36,6 @@ namespace Com.MyCompany.MyGame
         void Start()
         {
             unit = gameObject.GetComponent<Unit>();
-            rigidBody = gameObject.GetComponent<Rigidbody>();
 
             playerSpeed = unit.speed;
 
@@ -52,30 +50,26 @@ namespace Com.MyCompany.MyGame
 
         void FixedUpdate()
         {
-            if (Mathf.Abs(Input.GetAxis("Vertical")) > 0.2 || Mathf.Abs(Input.GetAxis("Horizontal")) > 0.2)
-            {
-                SetLookDir(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
-            }
-
             //플레이어 캐릭터 회전
+            SetLookDir(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
             transform.LookAt(lookDir, Vector3.up);
             transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);  //플레이어가 낙하할 때 x축 또는 z축이 회전하는 현상 방지, freezeRotation으로 제어 안 됨
 
             //플레이어 캐릭터 이동, 바닥과 접촉했을 때만 가능
             //바닥 접촉 문제 해결되면 숙이기 구현 -> 플레이어 이동속도 감소
-            //이후 은신 관련 기능 추가 (예시)))float cloak; if (플레이어 == 서있음) cloak = 1; else if (플레이어 == 숙이기) cloak = 0.5;)
+            //이후 은신 관련 기능 추가 (예시)))float cloak; if (플레이어 == 서있음) ctraloak = 1; else if (플레이어 == 숙이기) cloak = 0.5;)
             if (unit.isOnFloor)
             {
                 if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
                 {
-                    rigidBody.MovePosition(transform.position
-                                            + (mainCameraTransform.forward * Input.GetAxis("Vertical") + mainCameraTransform.right * Input.GetAxis("Horizontal"))
-                                            * playerSpeed * Time.deltaTime);
+                    destiPos = unit.GetDestiPos(transform.position, mainCameraTransform, Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+
+                    transform.position = Vector3.Lerp(transform.position, destiPos, 0.5f);
 
                     if (Input.GetAxis("Vertical") != 0)
-                        inertia = Input.GetAxis("Vertical") * 1.2f;
+                        inertia = Mathf.Abs(Input.GetAxis("Vertical")) * 1.2f;
                     else if (Input.GetAxis("Horizontal") != 0)
-                        inertia = Input.GetAxis("Horizontal") * 1.2f;
+                        inertia = Mathf.Abs(Input.GetAxis("Horizontal")) * 1.2f;
                 }
                 else
                 {
@@ -86,9 +80,9 @@ namespace Com.MyCompany.MyGame
             else if (!unit.isOnFloor && inertia != 0)
             {
                 //플레이어 캐릭터의 전방으로 관성을 준다
-                rigidBody.MovePosition(transform.position + transform.forward * inertia * playerSpeed * Time.deltaTime);
+                destiPos = unit.GetDestiPos(inertia, 0);
+                transform.position = Vector3.Lerp(transform.position, destiPos, 0.5f);
             }
-            Debug.Log(inertia);
         }
 
         #endregion
